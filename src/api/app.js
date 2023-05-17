493d871f3452e43d534419449330210dd23f41b6
Tarsis <br.tmvdl@gmail.com>
2023-05-17 16:44:34 -0300

    Fixing API files

--- src/api/app.js
+++ src/api/app.js
@@ -7,7 +7,22 @@ const log = new Logger('API')
 router.post('/games/list', (req, res) => {
   log.info('/games/list', { req, res })
   //
-  return res.setJSON({ list: [] })
+  return res.setJSON({
+    list: [
+      {
+        image: 'http://127.0.0.1:8080/img/game.png',
+        name: 'Game App',
+        price: 1.0,
+        link: 'http://127.0.0.1:8080/games/game-app/',
+      },
+      {
+        image: 'http://127.0.0.1:8080/img/game.png',
+        name: 'Game App 1',
+        price: 1.2,
+        link: 'http://127.0.0.1:8080/games/game-app-1/',
+      },
+    ]
+  })
 })
 
 module.exports = router
