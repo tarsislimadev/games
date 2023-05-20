a0ecc7052f0909dda6c17e6bab1cf482c4163f5c
Tarsis <br.tmvdl@gmail.com>
2023-05-20 11:15:40 -0300

    Fixing Chalkboard game files

--- src/www/games/chalkboard/js/game.js
+++ src/www/games/chalkboard/js/game.js
@@ -1,58 +1,113 @@
-import { nButton, nElement } from '../../../js/nElement.js'
+import { nButton, nElement, nFlex } from '../../../js/nElement.js'
 
 class Game {
   dom = null
-  numbers = null
+
+  els = {
+    lostsAndWins: null,
+    losts: null,
+    wins: null,
+    numbers: null,
+  }
+
+  state = {
+    nums: [],
+    wins: 0,
+    losts: 0,
+  }
 
   constructor({ dom }) {
     this.dom = dom
     //
-    const numbers = new nElement()
-    this.dom.append(this.numbers = numbers)
+    this.createLostsAndWins()
   }
 
-  createButton(n, max) {
-    const self = this
+  createLostsAndWins() {
+    this.dom.append(this.lostsAndWins = new nFlex())
+    this.lostsAndWins.setStyle('border-bottom', 'calc(1rem / 8) solid #000000')
 
-    const num = new nButton()
-    num.setText(n)
-    num.setContainerStyle('text-align', 'center')
-    num.setStyle('background-color', '#000000')
-    num.setStyle('margin', '1rem auto')
-    num.setStyle('color', '#FFFFFF')
-    num.setStyle('width', '10rem')
-
-    num.on('click', () => {
-      if (n == max) {
-        num.setStyle('background-color', '#FFFFFF')
-        num.setStyle('color', '#000000')
-      } else {
-        num.setStyle('background-color', '#FF0000')
-        num.setStyle('color', '#FFFFFF')
-      }
-
-      setTimeout(() => self.start(), 500)
+    const [losts, wins] = Array.from(Array(2)).map(() => {
+      const num = new nElement()
+      num.setStyle('padding', '1rem')
+      num.setStyle('text-align', 'center')
+      num.setContainerStyle('width', '50%')
+      //
+      num.setText('0')
+      //
+      return num
     })
 
-    self.numbers.append(num)
+    wins.setStyle('border-left', 'calc(1rem / 8) solid #000000')
+
+    this.lostsAndWins.append(this.els.losts = losts)
+    this.lostsAndWins.append(this.els.wins = wins)
+
+    this.updateLostsAndWins()
   }
 
-  createGame(nums = []) {
+  createNumbers() {
+    const maxNumber = 100
+    //
+    this.state.numbers = Array.from(Array(2))
+      .map(() => Math.floor(Math.random() * maxNumber) + 1)
+  }
+
+  appendNumbers() {
     const self = this
     //
-    self.numbers.clear()
+    if (!self.els.numbers) {
+      self.dom.append(self.els.numbers = new nElement())
+    }
     //
-    const max = Math.max(...nums)
-    nums.map((n) => self.createButton(n, max))
+    self.els.numbers.clear()
+    //
+    const { numbers } = self.state
+    const max = Math.max(...numbers)
+    //
+    numbers.map((num) => {
+      const numButton = new nButton()
+      //
+      numButton.setText(num)
+      //
+      numButton.setContainerStyle('text-align', 'center')
+      numButton.setStyle('background-color', '#000000')
+      numButton.setStyle('margin', '1rem auto')
+      numButton.setStyle('color', '#FFFFFF')
+      numButton.setStyle('padding', '1rem')
+      numButton.setStyle('width', '10rem')
+      //
+      numButton.on('click', () => {
+        console.log({ num, max })
+        //
+        if (num == max) {
+          numButton.setStyle('background-color', '#0000FF')
+          self.state.wins++
+        } else {
+          numButton.setStyle('background-color', '#FF0000')
+          self.state.losts++
+        }
+
+        self.updateLostsAndWins()
+
+        setTimeout(() => self.start(), 500)
+      })
+      //
+      self.els.numbers.append(numButton)
+    })
+  }
+
+  updateLostsAndWins() {
+    this.els.losts.setText(this.state.losts)
+    //
+    this.els.wins.setText(this.state.wins)
   }
 
   start() {
-    const nums = Array.from(Array(2)).map(() => Math.floor(Math.random() * 10) + 1)
+    this.createNumbers()
     //
-    console.log({ nums })
-    //
-    this.createGame(nums)
+    this.appendNumbers()
   }
+
 }
 
 export default Game
